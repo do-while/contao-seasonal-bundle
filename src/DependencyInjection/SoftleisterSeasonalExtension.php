399fed1cc89637d6961c25069ed44d82ee6fa346
do-while <info@softleister.de>
2025-02-08 11:10:39 +0100

    jetzt ist der richtige Name von Datei und Klasse gefunden!

--- src/DependencyInjection/SoftleisterSeasonalExtension.php
+++ src/DependencyInjection/SoftleisterSeasonalExtension.php
@@ -17,7 +17,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
-class SeasonalExtension extends Extension
+class SoftleisterSeasonalExtension extends Extension
 {
     public function load( array $configs, ContainerBuilder $container ): void
     {
